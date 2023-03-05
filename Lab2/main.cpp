b5540c57366e82da7e48e47dfb94e14ca6bcc093
Антон Жидович <toni.zhidovich@mail.ru>
2023-03-05 12:14:28 +0300

    average thread

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -4,7 +4,8 @@
 using std::cout;
 using std::cin;
 using std::endl;
-
+const int SLEEP_TIME_1 = 7;
+const int SLEEP_TIME_2 = 12;
 struct Args {
     int* arr;
     int size;
@@ -26,12 +27,26 @@ UINT WINAPI min_max(void* p) {
         if (arr[minInd] > arr[i]) {
             minInd = i;
         }
-        Sleep(7);
+        Sleep(SLEEP_TIME_1);
     }
     args->maxInd = maxInd;
     args->minInd = minInd;
     cout << "Max element: " << arr[maxInd] << endl;
     cout << "Min element: " << arr[minInd] << endl;
+    return 0;
+}
+
+UINT WINAPI average(void* p) {
+    Args* args = static_cast<Args*>(p);
+    int n = args->size;
+    int* arr = args->arr;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+        Sleep(SLEEP_TIME_2);
+    }
+    args->avg = sum / n;
+    printf("Average value: %d \n", args->avg);
     return 0;
 }
 
@@ -58,10 +73,24 @@ int main() {
     HANDLE hmin_max;
     hmin_max = (HANDLE)
         _beginthreadex(NULL, 0, min_max, args, 0, NULL);
-    if (hmin_max == NULL) {
+    if (NULL == hmin_max) {
+        return GetLastError();
+    }
+
+    HANDLE havg;
+    havg = (HANDLE)
+        _beginthreadex(NULL, 0, average, args, 0, NULL);
+    if (NULL == havg) {
         return GetLastError();
     }
 
     WaitForSingleObject(hmin_max, INFINITE);
+    WaitForSingleObject(havg, INFINITE);
+
+    arr[args->minInd] = arr[args->maxInd] = args->avg;
+    cout << "\nChanged array: " << endl;
+    printArr(arr, n);
+    delete args;
+    delete[] arr;
     return 0;
 }
